a45e4fac214e1b5ed9c533108783bb345c396cd8
Your Name <you@example.com>
2020-12-07 10:15:41 -0400

    Modelo

---
+++ src/js/Home.vue
@@ -0,0 +1,35 @@
+<template>
+	<div>
+		<span>Página HOME: {{ t }}</span>
+
+		<ul>
+			<li>
+				<router-link to="/user/1">Usuário 1</router-link> -
+				<router-link to="/user/1/perfil">Perfil</router-link> -
+				<router-link to="/user/1/posts">Posts</router-link>
+			</li>
+			<li>
+				<router-link to="/user/2">Usuário 2</router-link> -
+				<router-link to="/user/2/perfil">Perfil</router-link> -
+				<router-link to="/user/2/posts">Posts</router-link>
+			</li>
+		</ul>
+		
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+		t:"Carlos Sampaio"
+		}
+	}
+}
+</script>
+
+<style>
+	span {
+		color: #FF0000;
+	}
+</style>
